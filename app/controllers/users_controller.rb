adf5bf7c9ff538e952f37f9c58c2a492f5b7940f
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-08-28 19:15:33 +0900

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new()
+  end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    redirect_to "/users"
+  end
+
+  private
+
 end
