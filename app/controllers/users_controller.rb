d35951f3156b10b3d19bfe9fe03fd9536f6d904b
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-09-06 15:25:48 +0900

    デフォルト画像設定OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,25 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new()
+    @user = User.new(user_params)
+    if params[:image_name]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image_name]
+      File.binwrite("app/assets/images/user_images/#{@user.image_name}", image.read)
+    end
+
+    if @user.save
+      redirect_to users_url
+    else
+      render :new
+    end
   end
   
   def show
     @user = User.find_by(id: params[:id])
+  end
+
+  def edit
   end
 
   def destroy
@@ -22,5 +36,9 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :image_name)
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_134903) do
+ActiveRecord::Schema.define(version: 2020_09_06_053455) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_name"
   end
 
 end
